9282f74e5f16dbac442a5ea6b565061190a69438
Patriasoul <patriasoul@protonmail.com>
2026-09-07 21:32:06 +0200

    Poboljšaj glavnu navigaciju s aktivnim linkovima

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,14 +1,14 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Header({ itemCount }) {
   return (
     <header className="header">
       <Link className="brand" to="/">Art<span>Kolač</span></Link>
       <nav>
-        <Link to="/ponuda">Ponuda</Link>
-        <Link to="/torta-po-zelji">Torta po želji</Link>
-        <Link to="/o-nama">O nama & Kontakt</Link>
-        <Link className="cart" to="/narudzba">Košarica ({itemCount})</Link>
+        <NavLink to="/ponuda">Ponuda</NavLink>
+        <NavLink to="/torta-po-zelji">Torta po želji</NavLink>
+        <NavLink to="/o-nama">O nama & Kontakt</NavLink>
+        <NavLink className="cart" to="/narudzba">Košarica ({itemCount})</NavLink>
       </nav>
     </header>
   )
