6f21eba5b1b402c180014ca4d9da74253590b29d
Patriasoul <patriasoul@protonmail.com>
2026-09-07 21:18:27 +0200

    ArtKolač: izdvoji katalog proizvoda u zasebnu datoteku

---
+++ frontend/src/data/products.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Čokoladna torta',
+    category: 'Torte',
+    price: 32,
+    description: 'Bogata domaća čokoladna torta, izrađena svježe po narudžbi.'
+  },
+  {
+    id: 2,
+    name: 'Voćna torta',
+    category: 'Torte',
+    price: 30,
+    description: 'Lagani biskvit, krema i sezonsko voće.'
+  },
+  {
+    id: 3,
+    name: 'Domaći kremasti kolači',
+    category: 'Kolači',
+    price: 18,
+    description: 'Izbor svježih kremastih kolača za svaku prigodu.'
+  },
+  {
+    id: 4,
+    name: 'Sitni kolači',
+    category: 'Sitni kolači',
+    price: 16,
+    description: 'Pažljivo izrađen izbor domaćih sitnih kolača.'
+  }
+]
