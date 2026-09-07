a023aede0b1e332d1dbae438c976c9ab9efb75c4
Patriasoul <patriasoul@protonmail.com>
2026-09-07 21:38:37 +0200

    Add protected admin routes and admin login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
+import ProtectedAdminRoute from './components/layout/ProtectedAdminRoute'
 import HomePage from './pages/HomePage'
 import OrderPage from './pages/OrderPage'
 import CustomOrderPage from './pages/CustomOrderPage'
 import AboutContactPage from './pages/AboutContactPage'
+import AdminLoginPage from './pages/AdminLoginPage'
 import AdminDashboardPage from './pages/AdminDashboardPage'
 import AdminPage from './pages/AdminPage'
 import AdminCatalogPage from './pages/AdminCatalogPage'
@@ -24,12 +26,15 @@ function App() {
       <Route path="/torta-po-zelji" element={<CustomOrderPage />} />
       <Route path="/narudzba" element={<OrderPage cart={cartState.cart} subtotal={cartState.subtotal} onRemove={cartState.removeFromCart} onQuantityChange={cartState.changeQuantity} onClear={cartState.clearCart} />} />
       <Route path="/o-nama" element={<AboutContactPage />} />
-      <Route path="/admin" element={<AdminDashboardPage />} />
-      <Route path="/admin/narudzbe" element={<AdminPage />} />
-      <Route path="/admin/ponuda" element={<AdminCatalogPage />} />
-      <Route path="/admin/kalendar" element={<AdminCalendarPage />} />
-      <Route path="/admin/postavke" element={<AdminSettingsPage />} />
-      <Route path="/admin/torte-po-zelji" element={<AdminCustomOrdersPage />} />
+      <Route path="/admin/login" element={<AdminLoginPage />} />
+      <Route element={<ProtectedAdminRoute />}>
+        <Route path="/admin" element={<AdminDashboardPage />} />
+        <Route path="/admin/narudzbe" element={<AdminPage />} />
+        <Route path="/admin/ponuda" element={<AdminCatalogPage />} />
+        <Route path="/admin/kalendar" element={<AdminCalendarPage />} />
+        <Route path="/admin/postavke" element={<AdminSettingsPage />} />
+        <Route path="/admin/torte-po-zelji" element={<AdminCustomOrdersPage />} />
+      </Route>
       <Route path="*" element={<HomePage products={products} onAdd={cartState.addToCart} />} />
     </Routes>
     <Footer />
